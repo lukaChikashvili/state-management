c8e585fea047b237648fc39939a0aa549be3db28
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-05 13:36:09 +0400

    add counter state managment using zustand

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,11 +1,15 @@
-
+import { useStore } from "./store/store"
 
 function App() {
+  
+  const { count, increaseCount, decreaseCount } = useStore();
 
-
+    
   return (
     <>
-     
+     <p>{count}</p>
+     <button onClick={increaseCount}>increase</button>
+     <button onClick={decreaseCount}>decrease</button>
     </>
   )
 }
